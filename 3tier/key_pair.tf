4f161e22ce1a17958a037f1a7534795c97572223
Obiana Ifeanyi <Ifeanyiobiana@gmail.com>
2023-11-25 12:16:48 +0100

    Delete 3tier/key_pair.tf

--- 3tier/key_pair.tf
+++
@@ -1,5 +0,0 @@
-# Create a key pair for EC2 instances
-resource "aws_key_pair" "example" {
-  key_name   = "example-key"
-  public_key = file("~/.ssh/id_rsa.pub") # Replace with the path to your public key
-}
